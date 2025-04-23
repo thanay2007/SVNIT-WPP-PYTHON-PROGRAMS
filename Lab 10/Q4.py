94a13e17c7fb49c4398e64f2d2ffd457dcaa33be
Thanay Bodda <thanaybodda@gmail.com>
2025-04-23 22:21:23 +0530

    Create Q4.py

---
+++ Lab 10/Q4.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+N = 10  
+cartesian_points = np.random.rand(N, 2) * 10  
+
+x = cartesian_points[:, 0]
+y = cartesian_points[:, 1]
+r = np.sqrt(x**2 + y**2)
+theta = np.arctan2(y, x)
+
+polar_points = np.column_stack((r, theta))
+
+print("Cartesian Points:")
+print(cartesian_points)
+print("\nPolar Coordinates (r, θ):")
+print(polar_points)
